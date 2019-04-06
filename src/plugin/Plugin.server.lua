635d812a3dc152f69eeeba4fd2286f5859f88c5f
Anthony <obrienanthony13@gmail.com>
2019-04-06 22:19:32 +0100

    plugin fix

--- src/plugin/Plugin.server.lua
+++ src/plugin/Plugin.server.lua
@@ -3,25 +3,22 @@
     Written by Sublivion
 --]]
 
-MOD_TEMPLATE = 
-    [[planet = require(3044032874)
-    exports = {}
+MOD_TEMPLATE = [[planet = require(3044032874)
+exports = {}
 
-    return exports
-    ]]
+return exports]]
 
-SCRIPT_TEMPLATE = 
-    [[planet = require(3044032874)
-    print("Hello planet!")
-    ]]
+SCRIPT_TEMPLATE = [[planet = require(3044032874)
+print("Hello planet!")
+]]
 
 DEF_MOD = [[local module = {}
 
-    return module
-    ]]
+return module
+]]
 
 DEF_SCRIPT = [[print("Hello world!")
-    ]]
+]]
 
 game.DescendantAdded:Connect(function(newScript)
     if newScript:IsA('LuaSourceContainer') then
